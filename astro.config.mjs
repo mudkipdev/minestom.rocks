8500ed57ee42535f53251ed1fbea511829f6da64
mudkipdev <mudkip@mudkip.dev>
2026-05-06 14:06:37 -0600

    set trailingSlash = ignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
     site: "https://minestom.rocks",
+    trailingSlash: "ignore",
     devToolbar: {
         enabled: false
     }
